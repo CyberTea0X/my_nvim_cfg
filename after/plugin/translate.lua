a4df133c1adc3e54b590cf8995159a8ebc69bf11
unknown <mihailusov_778@mail.ru>
2023-07-09 15:03:11 +0300

    updated translation hotkeys

--- after/plugin/translate.lua
+++ after/plugin/translate.lua
@@ -5,6 +5,8 @@ require("translate").setup({
         parse_before = "trim",
     },
 })
-vim.keymap.set("v", "<leader>t", ':Translate EN<CR>')
-vim.keymap.set("n", "<leader>t", ':Translate EN<CR>')
+vim.keymap.set("v", "<leader>te", ':Translate EN<CR>')
+vim.keymap.set("n", "<leader>te", ':Translate EN<CR>')
+vim.keymap.set("v", "<leader>tr", ':Translate RU<CR>')
+vim.keymap.set("n", "<leader>tr", ':Translate RU<CR>')
 -- Meow
