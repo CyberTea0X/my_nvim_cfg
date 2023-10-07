55bd140d18345e9b3617eb6fa89283b16ab61d96
CyberTea0X <mihailusov_778@mail.ru>
2023-10-08 02:35:08 +0300

    Cleanup

--- after/plugin/lsp_format.lua
+++ after/plugin/lsp_format.lua
@@ -5,13 +5,3 @@ require("lsp-format").setup {
         end,
     }
 }
--- require("lspconfig").gopls.setup { on_attach = on_attach }
--- require("lspconfig").jsonls.setup { on_attach = on_attach }
--- require("lspconfig").html.setup { on_attach = on_attach }
--- require("lspconfig").cssls.setup { on_attach = on_attach }
--- require("lspconfig").tsserver.setup { on_attach = on_attach }
--- require("lspconfig").lua_ls.setup { on_attach = on_attach }
--- require("lspconfig").pylsp.setup { on_attach = on_attach }
--- require("lspconfig").rust_analyzer.setup { on_attach = on_attach }
--- require("lspconfig").gopls.setup { on_attach = on_attach }
--- require("lspconfig").svelte.setup { on_attach = on_attach }
